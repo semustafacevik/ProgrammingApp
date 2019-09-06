3e508967f0b4a8c320ad7b6f95d44ef7957799d5
Mustafa_CEVIK <cevikksoft@gmail.com>
2019-09-06 15:45:06 +0300

    Add data access layer and add CRUD operations class.

---
+++ Programming.DAL/LanguagesDAL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.DAL
+{
+    public class LanguagesDAL
+    {
+        ProgrammingDBEntities db = new ProgrammingDBEntities();
+
+        public IEnumerable<Languages> GetAllLanguages()
+        {
+            return db.Languages;
+        }
+
+        public Languages GetLanguageByID(int ID)
+        {
+            return db.Languages.Find(ID);
+        }
+
+        public Languages CreateLanguage(Languages newLanguage)
+        {
+            db.Languages.Add(newLanguage);
+            db.SaveChanges();
+            return newLanguage;
+        }
+
+        public Languages UpdateLanguage(int ID, Languages updLanguage)
+        {
+            db.Entry(updLanguage).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return updLanguage;
+        }
+
+        public void DeleteLanguage(int ID)
+        {
+            db.Languages.Remove(db.Languages.Find(ID));
+            db.SaveChanges();
+        }
+    }
+}
